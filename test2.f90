f3ca41216f20adaf69349c7ce3d58079995cd888
Masaya ITO <masaya.ito@univ-rennes1.fr>
2020-03-19 13:50:16 +0900

    masa982 fixed test2.f90

--- test2.f90
+++ test2.f90
@@ -4,8 +4,8 @@ program main
 
   integer, parameter                                      :: double = selected_real_kind ( 9 )  
   integer :: i, j, k, nn
-  real ( kind = double ) :: hi, hi2, hi3
-  real ( kind = double ), allocatable, dimension ( : ) :: xx, yy, bb, chk
+  real ( kind = double ) :: hi, hi2, hi3, dx, aj, aj2, bj, dj
+  real ( kind = double ), allocatable, dimension ( : ) :: xx, yy, bb, chk, x, y
   real ( kind = double ), allocatable, dimension ( :, : ) :: AA, AA_inv
 
   nn = 11
@@ -15,6 +15,8 @@ program main
   allocate ( bb ( 1:nn ) ) 
   allocate ( chk ( 1:nn ) ) 
   allocate ( AA ( 1:nn, 1:nn ) ) 
+  allocate ( x ( 1:100 ) ) 
+  allocate ( y ( 1:100 ) ) 
   AA = 0
   bb = 0
   
@@ -47,12 +49,43 @@ program main
         chk ( i ) = chk ( i ) + AA_inv ( i, j ) * bb ( j )
      end do
   end do
-  
-  print *, "chk"
-  do i = 1, nn
-     write ( unit = 6, fmt = ' ( f20.10 ) ', advance = "yes" ) chk ( i )
+
+  dx = 0.1
+  do i = 1, 100
+     x ( i ) =  dx
+     do j = 2, nn
+        if ( xx ( j - 1 ) <= x ( i ) .and. x ( i ) <= xx ( j ) ) then
+           aj  = yy ( j -1 )
+           aj2 = yy ( j )
+           bj  = ( aj2 - aj ) / ( xx ( j ) - xx ( j -1 ) ) & 
+                 & - ( xx ( j ) - xx ( j -1 ) ) * ( chk ( j ) + 2 * chk ( j - 1 ) ) / 3
+           dj  = ( chk ( j ) - chk ( j - 1 ) ) / 3 * ( xx ( j ) - xx ( j - 1 ) )
+
+           y ( i ) = aj + bj * ( x ( i ) - xx ( j - 1 ) ) + chk ( j - 1 ) * ( x ( i ) - xx ( j - 1 ) ) ** 2 & 
+                      & + dj * ( x ( i ) - xx ( j - 1 ) ) ** 3
+
+        end if
+     end do     
+     dx = dx + 0.1
   end do
 
+  print *, "x"
+  open ( unit = 22, file = "data_result.dat", action = "write", status = "replace" )
+  do i = 1, 100
+     write ( unit = 22, fmt = ' ( 2f20.10 ) ' ) x ( i ), y ( i )
+  end do
+  close ( unit = 22 )
+
+!  print *, "y"
+!  do i = 1, 100
+!     write ( unit = 6, fmt = ' ( f20.10 ) ', advance = "yes" ) y ( i )
+!  end do
+
+!  print *, "chk"
+!  do i = 1, nn
+!     write ( unit = 6, fmt = ' ( f20.10 ) ', advance = "yes" ) chk ( i )
+!  end do
+!
 
 
   
@@ -66,7 +99,7 @@ program main
 !        end if
 !     end do
 !  end do
-!
+
 
   
 !  do i = 1, nn
